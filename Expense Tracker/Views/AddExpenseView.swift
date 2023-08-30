bcacc50bb2a25b821b7187659872e7e184488b45
lironasaraf <lironi06@gmail.com>
2023-08-30 09:39:15 +0300

    add expense view

--- Expense Tracker/Views/AddExpenseView.swift
+++ Expense Tracker/Views/AddExpenseView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct AddExpenseView: View {
+    
+    @State private var name = ""
+    @State private var amount = ""
+    @State private var isExpenseAdded = false
+    @State private var selectedCategory: String? = nil
+    let catagories = ["Food", "Housing", "Transport", "Entertainment", "Utilities"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 20){
+                TextField("Name of Expense", text: $name)
+                    .padding()
+                    .foregroundColor(.secondary)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                TextField("Amount", text: $amount)
+                    .padding()
+                    .foregroundColor(.secondary)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                LazyVGrid(columns: [
+                    GridItem(.flexible(),spacing: 16),
+                    GridItem(.flexible(),spacing: 16),
+                    GridItem(.flexible(),spacing: 16)
+                    
+                ], spacing: 16){
+                    ForEach(catagories[0...2], id: \.self) {
+                        category in
+                        Button {
+                            selectedCategory = category
+                        } label: {
+                            Text(category)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .navigationTitle("Add Expense")
+//            .preferredColorScheme(.dark)
+        }
     }
 }
 
